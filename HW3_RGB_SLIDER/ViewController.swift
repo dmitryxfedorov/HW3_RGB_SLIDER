edca6a80b528a6ab58b3dbf271cc3fafb66ad528
Dmitry <gtfckt89@gmail.com>
2022-06-24 16:23:48 +0300

    Initial Commit

---
+++ HW3_RGB_SLIDER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW3_RGB_SLIDER
+//
+//  Created by Dmitry on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
